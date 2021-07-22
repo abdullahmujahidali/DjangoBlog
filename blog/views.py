c1582679f37841756071a59a4f7120cbcb29c416
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2021-07-22 16:15:55 +0500

    Setting up template and BS class

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Abdullah Mujahid',
+        'title': 'Blog Post',
+        'content': 'First Post',
+        'date_posted': 'August 27,1998'
+    },
+    {
+        'author': 'Saima Khan',
+        'title': 'Post Test',
+        'content': '2nd in line ',
+        'date_posted': 'July 27,2011'
+    },
+]
+
 def home(request):
-    return HttpResponse('<h1> Blog Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request,'blog/about.html',{'title': 'About'})
